5e358ec384400ffebd3af8f935fcadc5501dd731
raxelf <frananda.rafael@gmail.com>
2025-11-07 22:44:10 +0700

    feat: add exchange card

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import MainHeader from "@/components/MainHeader";
-import MarketCard from "@/components/MarketCard";
 import MarketCardContainer from "@/components/MarketCardContainer";
 import SideBar from "@/components/SideBar";
-import TransactionComponent from "@/components/TransactionComponent";
 import TransactionList from "@/components/TransactionList";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [coinQty, setCoinQty] = useState(10);
+  const [usdQty, setUsdQty] = useState(1028.9);
+
   return (
     // outside container
     <div className="grid grid-cols-12 w-full h-screen overflow-hidden">
@@ -97,7 +101,70 @@ export default function Home() {
         </section>
 
         {/* trading chart */}
-        <section></section>
+        <section className="grid grid-cols-7 mt-6 gap-4">
+          <div className="col-span-4 w-full h-full bg-[#2A2A2D] p-4 rounded-xl">
+            a
+          </div>
+
+          {/* Exchange Card */}
+          <div className="col-span-3 w-full h-full bg-[#2A2A2D] p-6 rounded-xl">
+            <p className="text-xl">Exchange</p>
+
+            <div className="flex gap-2 items-center justify-center text-xl mt-6">
+              <p>1</p>
+              <p className="text-gray-400">NTC</p>
+              <i className="ri-arrow-right-line"></i>
+              <p>$ 102.89</p>
+              <p className="text-gray-400">USD</p>
+            </div>
+
+            <div className="px-8 mt-4 flex flex-col gap-2">
+              <div className="w-full bg-[#645F5F] rounded-2xl px-4 py-2 text-black flex gap-2 justify-between">
+                <input
+                  type="number"
+                  name="buy"
+                  value={coinQty}
+                  onChange={(e) => setCoinQty(Number(e.target.value))}
+                  className="w-full"
+                />
+                <span className="flex items-center cursor-pointer">
+                  <Image
+                    src={"/nitecoin.png"}
+                    width={32}
+                    height={32}
+                    alt={"Nitecoin"}
+                    className="flex"
+                  />
+                  <i className="ri-arrow-down-s-line"></i>
+                </span>
+              </div>
+
+              <div className="w-full bg-[#645F5F] rounded-2xl px-4 py-2 text-black flex gap-2 justify-between">
+                <input
+                  type="number"
+                  name="buy"
+                  value={usdQty}
+                  onChange={(e) => setUsdQty(Number(e.target.value))}
+                  className="w-full"
+                />
+                <span className="flex items-center cursor-pointer">
+                  <Image
+                    src={"/usd.png"}
+                    width={32}
+                    height={32}
+                    alt={"USD"}
+                    className="flex"
+                  />
+                  <i className="ri-arrow-down-s-line"></i>
+                </span>
+              </div>
+
+              <button className="bg-secondary w-full mt-4 rounded-2xl font-semibold px-4 py-3 cursor-pointer text-black">
+                EXCHANGE
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
